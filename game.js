80f6e5c37f385271815b675c284881151c957a50
unknown <salihbezai98@gmail.com>
2021-03-10 22:44:33 +0100

    adding some updates

---
+++ game.js
@@ -0,0 +1,24 @@
+var character = document.getElementById('character');
+var block = document.getElementById('block')
+
+const jump=()=>{
+    if (character.classList != 'animate'){
+        character.classList.add('animate')
+    }
+    setTimeout(()=>{
+    character.classList.remove('animate')
+    },500)
+}
+
+const checkDead=()=>{
+    setInterval(()=>{
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
+    if(blockLeft < 20 && blockLeft > 0 && characterTop >= 130){
+        block.style.animation='none'
+        block.style.display='none'
+        alert('you lose')
+    }
+    },1)
+}
+checkDead()
